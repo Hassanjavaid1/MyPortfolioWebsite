38c6059032bba346d1dec99972451a30c920f6f1
Hassanjavaid <hassanjavaid1214@gmail.com>
2023-10-28 23:52:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter,Routes,Route} from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/MyPortfolioWebsite">
         <Navbar />
         <div>
           <Routes>
